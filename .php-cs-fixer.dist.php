810e4b4b2c90985789afcf274350e598469e7093
Daniel Leech <dan.t.leech@gmail.com>
2023-03-06 23:25:47 +0000

    Update phpstan etc

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,59 @@
+<?php
+
+use PhpCsFixer\Finder;
+use PhpCsFixer\Config;
+
+$finder = Finder::create()
+    ->in([
+        __DIR__ . '/lib',
+        __DIR__ . '/tests',
+    ])
+;
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'void_return' => true,
+        'binary_operator_spaces' => [
+            'operators' => [
+                '=>' => null
+            ],
+        ],
+        'blank_line_before_statement' => [
+            'statements' => [
+                'break',
+                'continue',
+                'declare',
+                'default',
+                'do',
+                'exit',
+                'for',
+                'foreach',
+                'goto',
+                'if',
+                'include',
+                'include_once',
+                'require',
+                'require_once',
+                'return',
+                'switch',
+                'throw',
+                'try',
+                'while',
+                'yield',
+            ],
+        ],
+        'concat_space' => false,
+        'no_unused_imports' => true,
+        'php_unit_set_up_tear_down_visibility' => true,
+        'phpdoc_align' => [],
+        'phpdoc_indent' => false,
+        'phpdoc_separation' => true,
+        'no_superfluous_phpdoc_tags' => [
+            'allow_mixed' => true
+        ],
+        'fully_qualified_strict_types' => true,
+    ])
+    ->setFinder($finder)
+    ;
